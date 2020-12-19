88f8721f6739a5f6fc7f6d60f6e9e44a84bfdb47
Josue <73070103+joxpulp@users.noreply.github.com>
2020-12-18 23:57:07 -0500

    Add files via upload

---
+++ LEAPYEAR.py
@@ -0,0 +1,8 @@
+﻿año = int(input("Introduzca un año: "))
+
+if año<1582:
+    print ("No dentro del período del calendario gregoriano")
+elif año%4 != 0:
+    print ("Año común")
+else:
+    print ("Año bisiesto")
